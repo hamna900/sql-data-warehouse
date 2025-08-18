06a3062f9e4433244374bc6334199666afae0258
hamna900 <hamnaijaz410@gmail.com>
2025-08-18 22:05:31 +0500

    Update initialize_database.sql

--- scripts/initialize_database.sql
+++ scripts/initialize_database.sql
@@ -1,1 +1,22 @@
+/*
+This script sets up the initial Data Warehouse environment.
+*/
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    CREATE DATABASE DataWarehouse;
+END
+GO
+
+USE DataWarehouse;
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'Bronze') EXEC('CREATE SCHEMA Bronze');
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'Silver') EXEC('CREATE SCHEMA Silver');
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'Gold') EXEC('CREATE SCHEMA Gold');
+GO
 
